66ef2b60400b162823cf057cb5774192711067d9
Al-Kassim Kassim <alkassimk@gmail.com>
2021-05-10 14:25:09 +0100

    minor change

--- resolvers.js
+++ resolvers.js
@@ -11,7 +11,6 @@ const resolvers = {
 
         if (args.type === 'sell') {
           const sellMargin = btcUSD - (args.margin / 100) * btcUSD;
-          console.log(sellMargin);
           return sellMargin * args.exchangeRate;
         } else if (args.type === 'buy') {
           const buyMargin = btcUSD + (args.margin / 100) * btcUSD;
