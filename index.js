5de229f2097c14babfd439edc7dc16ef07ec6053
Al-Kassim Kassim <alkassimk@gmail.com>
2021-05-10 13:08:50 +0100

    fix btcUSD type conversion

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ const resolvers = {
         const { data } = await axios.get(
           'https://api.coindesk.com/v1/bpi/currentprice.json'
         );
-        const btcUSD = parseInt(data.bpi.USD.rate);
+        const btcUSD = parseFloat(data.bpi.USD.rate.replace(',', ''));
 
         if (args.type === 'sell') {
           const sellMargin = btcUSD - (args.margin / 100) * btcUSD;
+          console.log(sellMargin);
           return sellMargin * args.exchangeRate;
         } else if (args.type === 'buy') {
           const buyMargin = btcUSD + (args.margin / 100) * btcUSD;
